376c28faa79276b6d1dc3f9c59f3a585aa2f8882
Sourav Santosh Patil <67873791+sourav1859@users.noreply.github.com>
2025-02-12 12:15:42 -0500

    Create security-groups.tf

---
+++ security-groups.tf
@@ -0,0 +1,29 @@
+# Security group for HTTP and SSH access
+resource "aws_security_group" "my_sg" {
+  name        = "my_security_group"
+  description = "Allow HTTP and SSH traffic"
+
+  # Allow SSH access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP access
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
